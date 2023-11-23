54a5c15a904814cb455bd313380bb0e1be3cafba
nyikoskata <nykata04@gmail.com>
2023-11-23 11:20:28 +0100

    start

---
+++ src/App.ts
@@ -0,0 +1,10 @@
+﻿import http from "http";
+import content from "./Content";
+
+class Program {
+    constructor() {
+        http.createServer(content).listen(process.env.PORT || 8080);
+    }
+}
+
+new Program();
